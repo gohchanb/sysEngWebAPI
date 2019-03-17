bbd46db87ec94069a347acf0e67fe07f27870b31
gohchanb <gohchanb@hotmail.co.uk>
2019-03-17 18:38:47 +0000

    removed size global

--- testWebAPI.py
+++ testWebAPI.py
@@ -85,7 +85,6 @@ def sendData(conn, data):
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
-size = 1024
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
