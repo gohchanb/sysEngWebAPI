ec5d0d5c7d37ee78d079700e6b13970e4a215c60
gohchanb <gohchanb@hotmail.co.uk>
2019-03-17 19:20:12 +0000

    removed tf

--- testWebAPI.py
+++ testWebAPI.py
@@ -2,32 +2,32 @@ import os
 import time
 import numpy as np
 import cv2
-import tensorflow as tf
+# import tensorflow as tf
 
 import socket
 
-MODEL_NAME = 'object_detection/instruments_graph'
-
-CWD_PATH = os.getcwd()
-PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
-
-detection_graph = tf.Graph()
-with detection_graph.as_default():
-    od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-        serialized_graph = fid.read()
-        od_graph_def.ParseFromString(serialized_graph)
-        tf.import_graph_def(od_graph_def, name='')
-
-    sess = tf.Session(graph=detection_graph)
-
-image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
-num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-
-sessData = [detection_boxes, detection_scores, detection_classes, num_detections]
+# MODEL_NAME = 'object_detection/instruments_graph'
+#
+# CWD_PATH = os.getcwd()
+# PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
+#
+# detection_graph = tf.Graph()
+# with detection_graph.as_default():
+#     od_graph_def = tf.GraphDef()
+#     with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+#         serialized_graph = fid.read()
+#         od_graph_def.ParseFromString(serialized_graph)
+#         tf.import_graph_def(od_graph_def, name='')
+#
+#     sess = tf.Session(graph=detection_graph)
+#
+# image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+# detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+# detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+# detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+# num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+#
+# sessData = [detection_boxes, detection_scores, detection_classes, num_detections]
 
 def recieveDataOfSize(conn,size):
     byteCount = 0
