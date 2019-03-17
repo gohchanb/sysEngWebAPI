151c570771a402f4dd04e7278be033ad7448b266
gohchanb <gohchanb@hotmail.co.uk>
2019-03-17 19:17:43 +0000

    sending data added to loop

--- testWebAPI.py
+++ testWebAPI.py
@@ -2,32 +2,32 @@ import os
 import time
 import numpy as np
 import cv2
-# import tensorflow as tf
+import tensorflow as tf
 
 import socket
 
-# MODEL_NAME = 'object_detection/instruments_graph'
-#
-# CWD_PATH = os.getcwd()
-# PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
-#
-# detection_graph = tf.Graph()
-# with detection_graph.as_default():
-#     od_graph_def = tf.GraphDef()
-#     with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-#         serialized_graph = fid.read()
-#         od_graph_def.ParseFromString(serialized_graph)
-#         tf.import_graph_def(od_graph_def, name='')
-#
-#     sess = tf.Session(graph=detection_graph)
-#
-# image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-# detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-# detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-# detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
-# num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-#
-# sessData = [detection_boxes, detection_scores, detection_classes, num_detections]
+MODEL_NAME = 'object_detection/instruments_graph'
+
+CWD_PATH = os.getcwd()
+PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+    sess = tf.Session(graph=detection_graph)
+
+image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+sessData = [detection_boxes, detection_scores, detection_classes, num_detections]
 
 def recieveDataOfSize(conn,size):
     byteCount = 0
@@ -109,10 +109,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 if not fail:
                     frame = np.frombuffer(fullImage,dtype='uint8').reshape(shape)
 
-                    # boxes, scores, classes, num = sess.run(sessData, feed_dict={image_tensor:frame})
-
-                    msg = 'GOT IMAGE'
-                    conn.sendall(msg.encode('utf-8'))
+                    boxes, scores, classes, num = sess.run(sessData, feed_dict={image_tensor:frame})
+                    if not sendData(conn, boxes):
+                        print('ERROR in sending boxes')
+                    if not sendData(conn, classes):
+                        print('ERROR in sending data')
+                    if not sendData(conn, scores):
+                        print('ERROR in sending scores')
+                    # msg = 'GOT IMAGE'
+                    # conn.sendall(msg.encode('utf-8'))
                     # conn.sendall(data)
                 # tAfter = time.time()
                 # print("Time main: " + str(tAfter-tBefore))
