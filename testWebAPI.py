41e08309938b999577bcbf22da60bb14e76a091d
gohchanb <gohchanb@hotmail.co.uk>
2019-03-17 18:26:37 +0000

    bug fixes

--- testWebAPI.py
+++ testWebAPI.py
@@ -2,32 +2,32 @@ import os
 import time
 import numpy as np
 import cv2
-import tensorflow as tf
+# import tensorflow as tf
 
 import socket
 
-MODEL_NAME = 'object_detection/instruments_graph'
-
-CWD_PATH = os.getcwd()
-PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
-
-detection_graph = tf.Graph()
-with detection_graph.as_default():
-    od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-        serialized_graph = fid.read()
-        od_graph_def.ParseFromString(serialized_graph)
-        tf.import_graph_def(od_graph_def, name='')
-
-    sess = tf.Session(graph=detection_graph)
-
-image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
-num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-
-sessData = [detection_boxes, detection_scores, detection_classes, num_detections]
+# MODEL_NAME = 'object_detection/instruments_graph'
+#
+# CWD_PATH = os.getcwd()
+# PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
+#
+# detection_graph = tf.Graph()
+# with detection_graph.as_default():
+#     od_graph_def = tf.GraphDef()
+#     with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+#         serialized_graph = fid.read()
+#         od_graph_def.ParseFromString(serialized_graph)
+#         tf.import_graph_def(od_graph_def, name='')
+#
+#     sess = tf.Session(graph=detection_graph)
+#
+# image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+# detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+# detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+# detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+# num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+#
+# sessData = [detection_boxes, detection_scores, detection_classes, num_detections]
 
 def recieveDataOfSize(conn,size):
     byteCount = 0
@@ -49,6 +49,7 @@ def recieveDataOfSize(conn,size):
         fail = 1
         msg = 'ERROR sent too much data'
         conn.sendall(msg.encode('utf-8'))
+    return fullData, fail
 
 def shapeToString(shape):
     s = ''
@@ -60,7 +61,7 @@ def stringToShape(s):
     shape = ()
     nums = s.split(',')
     for i in nums:
-        shape += (int(nums),)
+        shape += (int(i),)
     return shape
 
 # def sendData(conn, data):
@@ -113,9 +114,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 fullImage, fail = recieveDataOfSize(conn, size)
 
                 if not fail:
-                    frame = np.frombuffer(fullImage,dtype=int).reshape(shape)
+                    frame = np.frombuffer(fullImage,dtype='uint8').reshape(shape)
 
-                    boxes, scores, classes, num = sess.run(sessData, feed_dict={image_tensor:frame})
+                    # boxes, scores, classes, num = sess.run(sessData, feed_dict={image_tensor:frame})
 
                     msg = 'GOT IMAGE'
                     conn.sendall(msg.encode('utf-8'))
