2cfc05bb2304516f0bf0e3080b44fa4658726a3a
gohchanb <gohchanb@hotmail.co.uk>
2019-03-17 19:20:30 +0000

    removed random

--- testAPI.py
+++ testAPI.py
@@ -21,7 +21,6 @@ def getDetectionData():
     frame = np.asarray(frame_in_json)
 
     boxes = [[0.0]*4]*200
-<<<<<<< HEAD
     boxes[0] = [0.18,0.05,0.9,0.35]
     boxes[1] = [0.2,0.4,0.99,0.67]
     boxes[2] = [0.09,0.74,0.93,0.92]
@@ -30,7 +29,6 @@ def getDetectionData():
     scores[0] = 0.7
     # scores[1] = 0.7
     scores[2] = 0.7
-=======
     boxes[0] = [0.15,0.7,0.9,0.89]
     boxes[1] = [0.1,0.35,0.90,0.62]
     boxes[2] = [0.15,0.11,0.95,0.3]
@@ -39,7 +37,6 @@ def getDetectionData():
     scores[0] = 0.9
     # scores[1] = 0.9
     # scores[2] = 0.9
->>>>>>> bebaaddfa28938418801ff6168695af763726697
     scores = [scores]
     classes = [1.0]*200
     classes[1] = 2.0
